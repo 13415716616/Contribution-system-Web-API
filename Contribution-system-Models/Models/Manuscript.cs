22e6c5576666424cd7fa32b00a6ab1ef75b00302
柒小芝 <1020655014@qq.com>
2019-12-28 11:39:43 +0800

    怜春忽至恼忽去，至又无言去未闻

---
+++ Contribution-system-Models/Models/Manuscript.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contribution_system_Models.Models
+{
+public class Manuscript
+    {
+        public string Manuscript_Title { get; set; }
+        public string Manuscript_Etitle { get; set; }
+        public string Manuscript_Keyword { get; set; }
+        public string Manuscript_Abstract { get; set; }
+        public string Manuscript_Reference { get; set; }
+        public string Manuscript_Text { get; set; }
+    }
+}
---
+++ Contribution-system/Controllers/ManuscriptController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Contribution_system_Models.Models;
+
+namespace Contribution_system.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ManuscriptController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult AddManuscript(Manuscript manuscript)
+        {
+            Console.WriteLine(manuscript.ToString());
+            return Ok();
+        }
+    }
+}
